c76a988ee8d7dc016f55b1f9be3fe2a5a2a57100
Artur Sopov <tullers@mail.ru>
2022-05-14 15:49:01 +0300

    upd

--- server/controllers/cardController.js
+++ server/controllers/cardController.js
@@ -1,6 +1,9 @@
 const Card = require("../models/cardModel");
 const handlerFactory = require("./handlerFactory");
 
+// for an admin:
 exports.createCard = handlerFactory.createOne(Card);
+
+// for users:
 exports.getAllCards = handlerFactory.getAll(Card);
 exports.getCard = handlerFactory.getOne(Card);
