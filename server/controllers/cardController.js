f34a4ad1ae1b56e3efa8e9a4c7327a22c28f9f63
Artur Sopov <tullers@mail.ru>
2022-05-14 13:13:42 +0300

    upd

--- server/controllers/cardController.js
+++ server/controllers/cardController.js
@@ -1,6 +1,13 @@
-exports.getAllCards = (req, res, next) => {
-  res.status(200).json({
-    status: "success",
-    data: "cards",
-  });
+const Card = require("../models/cardModel");
+
+exports.getAllCards = async (req, res, next) => {
+  try {
+    const data = await Card.find();
+    res.status(200).json({
+      status: "success",
+      data,
+    });
+  } catch (error) {
+    console.log(error.message);
+  }
 };
