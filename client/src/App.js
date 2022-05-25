ef89dafe9822125cf060614d5e9296abfb8be94c
Семен Пурнемцев <purnemtzev.semen@yandex.ru>
2022-05-25 07:19:01 +0300

    delete styled components

--- client/src/App.js
+++ client/src/App.js
@@ -2,8 +2,6 @@ import { useEffect, useState } from "react";
 import { getAllCards } from "./api/fetch";
 import React from "react";
 import { Route, Routes, BrowserRouter } from "react-router-dom";
-import GlobalStyles from "./GlobalStyles";
-import Theme from "./Theme";
 import { Layout } from "./components/Layout";
 
 const App = () => {
@@ -25,17 +23,14 @@ const App = () => {
     }, []);
 
     return (
-        <Theme themeMode={theme}>
-            <GlobalStyles />
-            <BrowserRouter>
-                <Routes>
-                    <Route path="/" element={<Layout />}>
-                        {/* <Route path="/" element={<Cards allFranchise={cards} />} /> */}
-                        <Route index element={<div>CARDS</div>} />
-                    </Route>
-                </Routes>
-            </BrowserRouter>
-        </Theme>
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Layout />}>
+                    {/* <Route path="/" element={<Cards allFranchise={cards} />} /> */}
+                    <Route index element={<div>CARDS</div>} />
+                </Route>
+            </Routes>
+        </BrowserRouter>
     );
 };
 
